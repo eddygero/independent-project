880e3ff40448ccf2b18e2c374eebbf36a02e174b
eddygero <gero.temitayo33@gmail.com>
2021-09-07 22:13:44 +0300

    updated commit

--- akan2.js/akan.js
+++ akan2.js/akan.js
@@ -1,12 +1,12 @@
 
 function getAkanName () {
-    let yearOfBirth = document.getElementById("year-input").value;
-    let monthOfBirth = Number(document.getElementById("month-input").value);
-    let dayOfBirth = Number(document.getElementById("day-input").value);
+    let yearOfBirth = document.getElementById("year").value;
+    let monthOfBirth = Number(document.getElementById("month").value);
+    let dayOfBirth = Number(document.getElementById("day").value);
   
     let genders = document.getElementsByName("gender");
   
-    // function to get gender
+    
     function getGender () {
       for (let gender of genders){
         if (gender.checked){
@@ -19,7 +19,7 @@ function getAkanName () {
   
     console.log(myGenderValue);
   
-    // validation functions
+    
     function monthValidator () {
       if (monthOfBirth < 1 || monthOfBirth > 12) {
         return false;
@@ -30,9 +30,9 @@ function getAkanName () {
   
     function dayValidator () {
       if (monthOfBirth === 2 && Number(yearOfBirth)%4 === 0) {
-        if (dayOfBirth > 28 || dayOfBirth < 1) {
+        if (dayOfBirth > 31 || dayOfBirth < 1) {
           return false;
-        } else if (monthOfBirth === 2 && dayOfBirth > 29) {
+        } else if (monthOfBirth === 2 && dayOfBirth > 31) {
           return false;
         } else if (monthOfBirth === 2 && dayOfBirth < 1) {
           return false;
@@ -46,24 +46,24 @@ function getAkanName () {
       }
     }
   
-    //validation variables
+    
     let monthValid = monthValidator();
     let dayValid = dayValidator();
   
-    //formula to determine day of birth (Sunday = 1, Monday = 2)etc..
+
     let dayOfWeekNumber = Math.floor((((Number(yearOfBirth.slice(0,2))/4)-2*Number(yearOfBirth.slice(0,2))-1)+
             ((5*Number(yearOfBirth.slice(2,4))/4))+((26*(monthOfBirth+1)/10))+dayOfBirth)%7);
   
-    //creating arrays of Akan names for males and females
+
     let maleAkanNames = [
-      "Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Kwaku", "Yaw", "Kofi", "Kwame"
-    ];
+      "Kwasi", "Kwadwo", "Kwabena", "Kwaku","Yaw", "Kofi", "Kwame"
+    ]
   
     let femaleAkanNames = [
       "Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"
-    ];
+    ]
   
-    //condition statement that validates input
+
     if (myGenderValue == "male" && monthValid && dayValid){
       switch (dayOfWeekNumber) {
         case 1:
@@ -154,9 +154,9 @@ function getAkanName () {
           return false;
           break;
         default:
-          alert("mmh no Akan name for you");
+          alert(" no Akan name");
         }
       } else{
-        alert("You entered invalid month or day");
+        alert(" invalid month or day");
       }
     }
